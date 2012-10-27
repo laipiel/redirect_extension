08173c58bfa141e76ee99b8a53c3c5eeb4853de0
Youngki Chang <laipiel@gmail.com>
2012-10-27 18:09:52 -0400

    Created functional options page
    Created options page.

--- options.js
+++ options.js
@@ -1,14 +1,41 @@
-var urls = {
-  'blockedUrls': [
-    'http://www.naver.com',
-    'http://www.daum.net'
-  ],
-  'redirectUrls': [
-    'http://www.theglobeandmail.com',
-    'http://www.economist.com',
-    'http://www.thestar.com',
-    'http://www.nationalpost.com'
-  ]
+var options = {};
+
+options.urls;
+
+options.populateUrls = function() {
+  if (!options.urls) {
+    return;
+  }
+  if (options.urls['blockedUrls']) {
+    var blockedUrlsTextArea = document.getElementById('blockedUrls');
+    blockedUrlsTextArea.value = options.urls['blockedUrls'].join('\n');
+  }
+  if (options.urls['redirectUrls']) {
+    var redirectUrlsTextArea = document.getElementById('redirectUrls');
+    redirectUrlsTextArea.value = options.urls['redirectUrls'].join('\n');
+  }
 };
 
-localStorage.setItem('enola', JSON.stringify(urls));
+options.saveUrls = function() {
+  var blockedUrlsTextArea = document.getElementById('blockedUrls');
+  options.urls['blockedUrls'] = blockedUrlsTextArea.value.split('\n');
+  var redirectUrlsTextArea = document.getElementById('redirectUrls');
+  options.urls['redirectUrls'] = redirectUrlsTextArea.value.split('\n');
+  localStorage.setItem('enola', JSON.stringify(options.urls));
+};
+
+options.init = function() {
+  options.populateUrls();
+  var saveButton = document.getElementById('save');
+  saveButton.addEventListener('click', options.saveUrls, false);
+};
+
+options.run = function() {
+  var urls = localStorage.getItem('enola');
+  if (urls) {
+    options.urls = JSON.parse(urls);
+  }
+  window.addEventListener('load', options.init, false);
+}
+
+options.run();
